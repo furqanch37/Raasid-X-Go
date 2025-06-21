6c7c4df9663979151b50ea7ba368734c071a552b
Muhammad Furqan <muhammadfurqanch517@gmail.com>
2025-06-21 14:14:51 +0500

    Update store.js

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,3 +1,4 @@
+// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cartSlice';
 import userReducer from './features/userSlice';
@@ -7,6 +8,14 @@ const store = configureStore({
     cart: cartReducer,
     user: userReducer,
   },
+});
+
+// 🔁 Save cart to localStorage on every state update
+store.subscribe(() => {
+  if (typeof window !== 'undefined') {
+    const state = store.getState();
+    localStorage.setItem('cartItems', JSON.stringify(state.cart.items));
+  }
 });
 
 export default store;
