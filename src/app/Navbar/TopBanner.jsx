89f5fe608ad2cf9fc58a0849694f7dae8f6ebe3b
Muhammad Furqan <muhammadfurqanch517@gmail.com>
2025-06-23 09:27:24 +0500

    Update TopBanner.jsx

--- src/app/Navbar/TopBanner.jsx
+++ src/app/Navbar/TopBanner.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 const TopBanner = () => {
   return (
-    <div className="top-banner">
-      FREE Delivery over ₹499. Fulfilled by Groceries. Flat $10 Instant Cashback on Wallet Payment
-    </div>
+    <div className="top-banner">Welcome to Raasid — Premium Ready-to-Eat Meals, Juices, and Spices Crafted with Excellence.</div>
+
   );
 };
 
