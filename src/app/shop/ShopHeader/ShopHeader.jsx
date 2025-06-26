99f175a823c2f93e43c586b8ed5b267bec1ab083
Muhammad Furqan <muhammadfurqanch517@gmail.com>
2025-06-26 11:26:49 +0500

    commit

--- src/app/shop/ShopHeader/ShopHeader.jsx
+++ src/app/shop/ShopHeader/ShopHeader.jsx
@@ -4,7 +4,7 @@ export default function ShopHeader() {
   return (
     <section className="shop-header">
       <div className="container-0">
-        <h1 className="shop-title">Shop</h1>
+        <h1 className="shop-title-1">Shop</h1>
       </div>
     </section>
   );
